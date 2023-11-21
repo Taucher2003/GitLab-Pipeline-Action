d20afcd9f9a246442779a3cd55a25704bfb9b109
Niklas van Schrick <mc.taucher2003@gmail.com>
2023-11-21 21:37:03 +0100

    Fix image reference in action

--- release.rb
+++ release.rb
@@ -15,7 +15,7 @@ File.write 'README.md', readme_for_version
 
 action = File.read 'action.yml'
 action_for_version = action.gsub("  image: 'Dockerfile'",
-                                 "  image: 'ghcr.io/taucher2003/gitlab-pipeline-action:#{version}'")
+                                 "  image: 'docker://ghcr.io/taucher2003/gitlab-pipeline-action:#{version}'")
 File.write 'action.yml', action_for_version
 
 system('git add README.md action.yml')
