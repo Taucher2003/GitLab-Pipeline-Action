1d8db67f88548efacb1c6373106327d9d7f705e9
Niklas van Schrick <mc.taucher2003@gmail.com>
2026-07-25 20:19:49 +0200

    Update GitLab Runner to version 19.2.0

--- lib/action/step/start_runner.rb
+++ lib/action/step/start_runner.rb
@@ -5,7 +5,7 @@ module GitlabPipelineAction
     class StartRunner < Base
       RunnerInvalid = Class.new(StandardError)
 
-      GITLAB_RUNNER_IMAGE = 'registry.gitlab.com/gitlab-org/gitlab-runner:v19.1.1'
+      GITLAB_RUNNER_IMAGE = 'registry.gitlab.com/gitlab-org/gitlab-runner:v19.2.0'
 
       def execute
         runner_config_template_path = "#{File.expand_path('../config', __dir__)}/gitlab_runner_config_template.toml"
